6765b543f854d33f6f554e18cb3f7b77adef9aff
Chris Wilson <sales@aussiepet.com.au>
2019-05-11 17:35:58 +1000

    Refactored Books Controller

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -5,17 +5,27 @@ namespace App\Http\Controllers;
 use App\Book;
 use App\Http\Requests\BookStoreRequest;
 use App\Http\Requests\BookUpdateRequest;
-use Illuminate\Http\Request;
 
 class BooksController extends Controller
 {
     public function store(BookStoreRequest $request)
     {
-        Book::create($request->validated());
+        return redirect(route('books.show', [
+            'book' => Book::create($request->validated()),
+        ]));
     }
 
     public function update(Book $book, BookUpdateRequest $request)
     {
         $book->update($request->validated());
+
+        return redirect(route('books.show', compact('book')));
+    }
+
+    public function destroy(Book $book)
+    {
+        $book->delete();
+
+        return redirect(route('books.index'));
     }
 }
