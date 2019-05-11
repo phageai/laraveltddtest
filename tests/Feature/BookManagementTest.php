3e908e12d9dadd5aabc6655e978a5c786ab19358
Chris Wilson <sales@aussiepet.com.au>
2019-05-11 11:45:21 +1000

    initial book tests

--- tests/Feature/BookManagementTest.php
+++ tests/Feature/BookManagementTest.php
@@ -2,20 +2,71 @@
 
 namespace Tests\Feature;
 
+use App\Book;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class ExampleTest extends TestCase
+class BookManagementTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    /** @test */
+    public function a_book_can_be_added_to_the_library()
+    {
+        $this->withoutExceptionHandling();
+
+        $response = $this->post('/books', [
+            'title' => 'Cool Book',
+            'author' => 'Chris',
+        ]);
+
+        $response->assertOk();
+
+        $this->assertCount(1, Book::all());
     }
+    
+    /** @test */
+    public function a_title_is_required_to_add_book()
+    {
+        $response = $this->post('/books', [
+            'title' => '',
+            'author' => 'Chris',
+        ]);
+
+        $response->assertSessionHasErrors('title');
+
+    }
+
+    /** @test */
+    public function an_author_is_required_to_add_book()
+    {
+        $response = $this->post('/books', [
+            'title' => 'Cool Book',
+            'author' => '',
+        ]);
+
+        $response->assertSessionHasErrors('author');
+    }
+
+    /** @test */
+    public function a_book_can_be_updated()
+    {
+        $this->withoutExceptionHandling();
+
+        $this->post('/books', [
+            'title' => 'Cool Book',
+            'author' => 'Chris',
+        ]);
+
+        $book = Book::first();
+
+        $response = $this->patch('/books/' . $book->id, [
+            'title' => 'New Title',
+            'author' => 'New Author',
+        ]);
+
+        $this->assertEquals('New Title', $book->fresh()->title);
+        $this->assertEquals('New Author', $book->fresh()->author);
+    }
+
 }
