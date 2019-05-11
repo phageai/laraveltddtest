f67bcfefa03b970701605b6d47cade490bdc8b86
Chris Wilson <sales@aussiepet.com.au>
2019-05-11 17:36:24 +1000

    Updated web routes
    Including authors routes.

--- routes/web.php
+++ routes/web.php
@@ -11,5 +11,11 @@
 |
 */
 
-Route::post('books', 'BooksController@store');
-Route::patch('books/{book}', 'BooksController@update');
+Route::get('books', 'BooksController@index')->name('books.index');
+Route::get('books/{book}', 'BooksController@show')->name('books.show');
+Route::post('books', 'BooksController@store')->name('books.store');
+Route::patch('books/{book}', 'BooksController@update')->name('books.update');
+Route::delete('books/{book}', 'BooksController@destroy')->name('books.delete');
+
+Route::get('authors/{author}', 'AuthorsController@show')->name('authors.show');
+Route::post('authors', 'AuthorsController@store')->name('authors.store');
